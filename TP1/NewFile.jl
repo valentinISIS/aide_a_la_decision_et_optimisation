ff58856b1430421f313309f3af00aa42b2fe3989
valentinLaptop <valentin.beaumelle@etud.univ-jfc.fr>
2026-09-08 18:03:30 +0200

    Implement dijkstra and test on small graph

--- TP1/NewFile.jl
+++ TP1/NewFile.jl
@@ -58,12 +58,13 @@ read_graph = function(filename::String)
         lines = readlines(io)
         nb_nodes = parse(Int64, lines[1])
         nb_relations = parse(Int64, lines[2])
-        al = [ Vector{Int64}() for _ in 1:nb_nodes]
+        al = [ Vector{Vector{Int64}}() for _ in 1:nb_nodes]
         for i in 3:nb_relations+2
             line = split(lines[i])
             val_start = parse(Int64, line[1])
             val_end = parse(Int64, line[2])
-            push!(al[val_start], val_end)
+            weight = parse(Int64, line[3])
+            push!(al[val_start], [val_end, weight])
         end
         return al
     end
@@ -110,3 +111,49 @@ end
 
 println("-----DFS-----")
 dfs(al, 1)
+
+dijkstra = function (graph, start_node, end_node)
+    
+    get_min_dist = function (stack, dist)
+        min = typemax(Int64)
+        best_node = 0
+        best_index = 0
+        for index in eachindex(stack)
+            node = stack[index]
+            if dist[node] < min
+                min = dist[node]
+                best_node = node
+                best_index = index
+            end
+        end
+        return best_node, best_index
+    end
+
+
+    dist = [ typemax(Int64) for _ in 1:length(graph)]
+    stack = [i for i in 1:length(graph)]
+    pred = [Vector{Int64}() for _ in 1:length(graph)]
+    dist[start_node] = 0
+
+    node = 0
+    while length(stack) > 0 & node != end_node
+        node, index = get_min_dist(stack, dist)
+        deleteat!(stack, index)
+        for neigbourgh in graph[node]
+            if dist[node] + neigbourgh[2] < dist[neigbourgh[1]]
+                dist[neigbourgh[1]] = dist[node] + neigbourgh[2]
+                pred[neigbourgh[1]] = copy(pred[node])
+                push!(pred[neigbourgh[1]], node)
+            end
+        end
+    end
+
+    println("Minimun dist equal : ", dist[end_node])
+    println("The best road found is :", pred[end_node])
+end
+
+println("-----Dijkstra-----")
+println("----Easy graph----")
+dijkstra(al_from_file, 1, 9)
+
+println("----Hard graph----") 
